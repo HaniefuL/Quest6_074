daef7dc50a0d6bf5c1f8e2aaaf38534c5eadb786
HaniefuL <haniefulgeofarry@gmail.com>
2025-11-20 14:17:09 +0700

    Menambahkan LocalContext untuk mengambil string resource dari JenisK

--- app/src/main/java/com/example/praktikum8/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/praktikum8/view/uicontroller/Navigasi.kt
@@ -1,9 +1,15 @@
 package com.example.praktikum8.view.uicontroller
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 @Composable
@@ -11,4 +17,25 @@ fun DataApp(
     modifier: Modifier,
     viewModel: SiswaViewModel = viewModel(),
     navController: NavHostController = rememberNavController(),
-)
+){
+    Scaffold { isiRuang ->
+        val uiState = viewModel.statusUI.collectAsState()
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+
+            modifier = Modifier.padding(isiRuang)){
+            composable(route = Navigasi.Formulirku.name){
+                val konteks = LocalContext.current
+                FormIsian (
+                    pilihanJK = JenisK.map { id -> konteks.resources.getString(id)},
+                    //pilihanJK = JenisK .map { id -> konteks.resroude.getstring}
+                    OnSubmitButtonClicked = {
+                        viewModel.setSiswa(it)
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+        }
+    }
+}
